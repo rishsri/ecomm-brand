13ee68414c1ef33bc550032e0531088cdf3384f2
rishsri <rishabhsri36@gmai.com>
2024-01-31 05:34:15 +0530

    header component

---
+++ src/pages/homePage/Home.jsx
@@ -0,0 +1,15 @@
+import TopNavbar from "../../components/header/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <TopNavbar />
+      {/* <HeaderSlider />
+      <AllCategories />
+      <LatestProducts /> */}
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default Home;
