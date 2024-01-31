1632e62bf18fffdcb0213a656589e7656dfba9ea
rishsri <rishabhsri36@gmai.com>
2024-01-31 05:42:07 +0530

    header slider

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,12 +1,13 @@
 import TopNavbar from "../../components/header/Navbar";
+import HeaderSlider from "../../components/slider/HeaderSlider";
 
 const Home = () => {
   return (
     <>
       <TopNavbar />
-      {/* <HeaderSlider />
-      <AllCategories />
-      <LatestProducts /> */}
+       <HeaderSlider />
+      {/* <AllCategories />
+      <LatestProducts />  */}
       {/* <Footer /> */}
     </>
   );
